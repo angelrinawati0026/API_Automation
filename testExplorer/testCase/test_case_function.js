1811aa051d26464b3ab8af8011e6a73d068d187a
Angeli <26angelsilaban@gmail.com>
2024-02-19 10:53:16 +0700

    create test case using function

--- testExplorer/testCase/test_case_function.js
+++ testExplorer/testCase/test_case_function.js
@@ -1,16 +1,22 @@
-const { getMethod, postMethod  ,putMethod } = require("../apiServer/apiObjects");
+const { postMethod  ,putMethod,getMethod,patchMethod } = require("../apiServer/apiObjects");
 
 //test suites
 describe("Testing API restful", function () {
   let id;
   // Test case 1
-  it("Test GET from Function", async function () {
-    await getMethod();
-  });
   it("Test POST from Function", async function () {
    id = await postMethod();
   });
   it("Test PUT from Function", async function () {
     await putMethod(id);
    });
+   it("Test GET from Function", async function () {
+    await getMethod(id);
+  });
+  it("Test PATCH from Function", async function () {
+    await patchMethod(id);
+   });
+   it("Test GET from Function", async function () {
+    await getMethod(id);
+  });
 });
